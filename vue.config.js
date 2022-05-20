8259c09f0ac8ad9e98c650b85d2fd6abedb4832a
刘富国 <595726017@qq.com>
2022-05-20 16:48:22 +0800

    fix: 排除.js.map

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const {defineConfig} = require('@vue/cli-service')
 
 module.exports = defineConfig({
     transpileDependencies: true,
+    //排除.js.map
+    productionSourceMap: process.env.NODE_ENV === 'development',
     configureWebpack: {
         plugins: [
             require('unplugin-element-plus/webpack')({
