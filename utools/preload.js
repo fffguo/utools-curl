e37e1c132bd2647c39cf7354b4450f2e9e8c0d47
liufuguo <595726017@qq.com>
2022-09-20 18:04:10 +0800

    fix: 修复curl解析相关问题

--- utools/preload.js
+++ utools/preload.js
@@ -13,27 +13,29 @@ args 示例结构
 callback参考：http://nodejs.cn/learn/the-nodejs-http-module#httpincomingmessage    http.IncomingMessage
 */
 // eslint-disable-next-line no-undef
-sendRequest = function (args, callback, errorCallback) {
+sendRequest = function (curl, callback, errorCallback) {
     try {
-        const myURL = new URL(args.url);
+        console.log("原始请求curl：", curl);
+        const myURL = new URL(curl.url);
+        handleHeader(curl.headers, curl.body);
         const options = {
             hostname: myURL.hostname,
-            port: myURL.port === "" ? (args.url.startsWith('https') ? 443 : 80) : myURL.port,
+            port: myURL.port === "" ? (curl.url.startsWith('https') ? 443 : 80) : myURL.port,
             path: myURL.pathname + myURL.search,
-            method: args.method,
-            headers: handleHeader(args.headers, args.body),
+            method: curl.method,
+            headers: curl.headers,
         };
         let request;
-        if (args.url.startsWith('https')) {
+        if (curl.url.startsWith('https')) {
             request = https.request(options, callback);
         } else {
             request = http.request(options, callback);
         }
-        if (args.body !== undefined) {
-            request.write(args.body);
+        if (curl.body !== undefined) {
+            request.write(curl.body);
         }
         console.log("请求参数options：", options);
-        console.log("body：", args.body);
+        console.log("body：", curl.body);
         request.on('error', errorCallback);
         request.end();
     } catch (e) {
@@ -43,16 +45,9 @@ sendRequest = function (args, callback, errorCallback) {
 }
 
 let handleHeader = function (headers, body) {
-    let newHeaders = {}
-    for (const key in headers) {
-        if ("Content-Type" === key && headers[key] === "application/x-www-form-urlencoded" && body !== undefined) {
-            newHeaders['Content-Length'] = Buffer.byteLength(body)
-        }
-        if ("Accept-Encoding" !== key) {
-            newHeaders[key] = headers[key];
-        }
+    if (body) {
+        headers['content-length'] = Buffer.byteLength(body)
     }
-    return newHeaders
 };
 
 // eslint-disable-next-line no-undef
